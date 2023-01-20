d796d925d65616bc32beeae1ecf4383380200c76
Progna_Labony_Roy <labony.progna@gmail.com>
2023-01-20 18:35:50 +0600

    contact me page created

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-hot-toast';
 
 const Contact = () => {
-    return (
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs
+      .sendForm(
+        "service_h8blw8w",
+        "template_mka0hfl",
+        form.current,
+        "AtXX8i6MtucT3D9P1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          toast.success("Email has sent successfully");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className='contact'>
+        <div className="one">
         <div>
-            
+          <p className="name">Sayesta Goswami, PhD </p>
+          <p className="tiny-text">CLINICAL PSYCHOLOGIST</p>
         </div>
-    );
+          <p className="footer-text">Anchal Tower, Sixmile, </p>
+          <p className="footer-text"> Guwahati, Assam 781022, India</p>
+        <p className="footer-text">+918333552221</p>
+        <p className="footer-text">sayestagoswami.pt@gmail.com</p>
+      </div>
+      <div className='contact-form'>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input className='submit-button' type="submit" value="Send" />
+    </form>
+  </div>
+    </div>
+ 
+  );
 };
 
 export default Contact;
